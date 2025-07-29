f3ab6d9d66f9b46b069f8e50431b94e16a6ec303
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2025-07-29 17:39:08 -0500

    feat: integrate React with Electron and set up Vite for development
    - Updated package.json to include React, ReactDOM, and Vite dependencies.
    - Added ESLint for code linting and TypeScript for type checking.
    - Created App component with a basic layout including a theme toggle and a financial summary table.
    - Added CSS styles for the application layout and components.
    - Set up Vite configuration for building and serving the application.
    - Created TypeScript configuration files for app and node environments.
    - Added SVG asset for React logo.

---
+++ src/UI/App.tsx
@@ -0,0 +1,68 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+            <nav className="Themes">
+                <div id="theme-toggle">
+                    <input type="checkbox" className="checkbox" id="checkbox" />
+                    <label htmlFor="checkbox" className="checkbox-label">
+                        <i className="fas fa-moon"></i>
+                        <i className="fas fa-sun"></i>
+                        <span className="ball"></span>
+                    </label>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <h1>Hello from Electron</h1>
+            <table id="tabla">
+                <thead>
+                    <tr>
+                        <th>Movimiento</th>
+                        <th>Fecha</th>
+                        <th>Monto</th>
+                        <th>Usuario</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Ingreso</td>
+                        <td>24-07-2024 12:00:00</td>
+                        <td>100000 ➕</td>
+                        <td>Sebastian</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Total: 100000</th>
+                        <td>➕ Nueva Transaccion</td>
+                    </tr>
+                </tfoot>
+            </table>
+            <article id="resume">
+                <h2>Resumen Financiero</h2>
+                <section>
+                    <h3>Sebastian</h3>
+                    <p>
+                        Sebastian lleva un registro positivo con un total de: 100000
+                    </p>
+                </section>
+            </article>
+        </main>
+        <footer>
+            <h2>Versiones:</h2>
+            <ul className="versionsList">
+                <li id="nodeVersion"></li>
+                <li id="chromeVersion"></li>
+                <li id="electronVersion"></li>
+            </ul>
+        </footer>
+        <script src="./renderer.js"></script>
+    </>
+  )
+}
+
+export default App
