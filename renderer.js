f301814e001466d30f998bbd95f605ac8e910e5f
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2025-08-13 21:20:02 -0500

    feat: enhance modal functionality and improve header navigation styles

--- renderer.js
+++ renderer.js
@@ -3,7 +3,7 @@ document.getElementById('chromeVersion').innerText = `Chrome: ${window.versions.
 document.getElementById('electronVersion').innerText = `Electron: ${window.versions.electron()}`;
 
 const checkTheme = async () => {
-    await window.darkMode.theme() ? document.getElementById('checkbox').checked = true : document.getElementById('checkbox').checked = false
+    await window.darkMode.theme() ? document.getElementById('theme-toggle').checked = true : document.getElementById('theme-toggle').checked = false
 }
 
 document.getElementById('notificationBtn').addEventListener('click', async () => {
@@ -12,17 +12,43 @@ document.getElementById('notificationBtn').addEventListener('click', async () =>
     }
 })
 
-document.getElementById('theme-toggle').addEventListener('click', async () => {
+// Logica para cambiar la clase de el Modal de "modal hidden" a "modal"
+document.getElementById("botonModal").addEventListener("click", () => {
+    document.getElementById("modal").classList.remove('hidden')
+})
+
+// Logica para agregarle la clase "hidden" a el modal
+function agregarClaseHidden() {
+    const modalElement = document.getElementById("modal").classList
+    if (modalElement.contains("hidden")) {
+        null
+    } else {
+        modalElement.add('hidden')
+    }
+}
+
+Array.from(document.getElementsByClassName("cerrarModal")).forEach(element => {
+    element.addEventListener('click', agregarClaseHidden)
+});
+
+
+document.getElementById('theme-toggle').addEventListener('change', async () => {
+
+    const isDarkMode = await window.darkMode.toggle()
+    console.log('Se cambio el tema a ', isDarkMode ? 'Dark' : 'Light')
+
+    // isDarkMode ? document.getElementById('theme-toggle').checked = true : document.getElementById('theme-toggle').checked = false
+
     // No se como funciona pero siempre entra 💀
     // Y lo que menos tiene sentido es que entra cuando `checked` no es true 🤷‍♂️.
 
     // Como sea, Esto soluciona el problema de que se active el evento de el click dos veces
     // Ni idea porque pasa eso.
-    if (document.getElementById('checkbox').checked) {
-        const isDarkMode = await window.darkMode.toggle()
-        console.log('Se cambio el tema a ', isDarkMode ? 'Dark' : 'Light', isDarkMode, ' Checked? ', document.getElementById('checkbox').checked)
-        isDarkMode ? document.getElementById('checkbox').checked = true : document.getElementById('checkbox').checked = false
-    }
+    // if (document.getElementById('checkbox').checked) {
+    //     const isDarkMode = await window.darkMode.toggle()
+    //     console.log('Se cambio el tema a ', isDarkMode ? 'Dark' : 'Light', isDarkMode, ' Checked? ', document.getElementById('checkbox').checked)
+    //     isDarkMode ? document.getElementById('checkbox').checked = true : document.getElementById('checkbox').checked = false
+    // }
 })
 
 checkTheme()
